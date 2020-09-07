d8a4edc4c92fb47a713adfbc72362c3007f27ca5
XuPeiYao <st89382000@gmail.com>
2020-09-07 16:47:00 +0800

    加入專案檔案。

---
+++ Lab_20200907_VideoStreaming/Controllers/VideoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Lab_20200907_VideoStreaming.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoController : ControllerBase
+    {
+        private readonly ILogger<VideoController> _logger;
+
+        public VideoController(ILogger<VideoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var videoFileStream =
+                System.IO.File.Open(
+                    "wwwroot/example.mp4",
+                    System.IO.FileMode.Open,
+                    System.IO.FileAccess.Read,
+                    System.IO.FileShare.Read);
+
+            return File(
+                fileStream: videoFileStream,
+                contentType: "video/mp4",
+                enableRangeProcessing: true);
+        }
+    }
+}
